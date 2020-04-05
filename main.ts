8cadbfe8be974b1db49eb364de2b532e631fe048
Parth Patel <parth_patel@comcast.com>
2020-04-05 19:33:46 -0400

    try with create method

--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
 import { Observable } from 'rxjs';
 
 let numbers = [1, 10, 20, 30];
-let source = Observable.from(numbers);
+let source = Observable.create(observer => {
+
+    numbers.forEach(element => {
+
+        // Try to print Error block of observable
+        if (element >= 5) {
+            observer.error("Element is Greater than expected!!!");
+        }
+        
+        observer.next(element);
+    })
+
+    observer.complete();
+});
 
 source.subscribe(
     value => console.log("This is Value:", value),
