791616e1f8f783f21a253de2a77e57696b66d91e
Parth Patel <parth_patel@comcast.com>
2020-04-05 19:19:28 -0400

    from operator

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-alert("Hello!!");
+import { Observable, Observer } from 'rxjs';
+
+let numbers = [1, 10, 20, 30];
+let source = Observable.from(numbers);
+
+class MyObserver implements Observer<number> {
+    
+    next(value) {
+        console.log("This is Value:", value);
+    }
+
+    error(e) {
+        console.log("This is Error", e);
+    }
+    
+    complete() {
+        console.log("This is completion of observable!");
+    }    
+}
+
+source.subscribe(new MyObserver());
