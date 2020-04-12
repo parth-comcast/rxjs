c19eafa080058b4531f7e564741a8643798e6b1f
Parth Patel <parth_patel@comcast.com>
2020-04-12 18:17:52 -0400

    implemention retry logic for network

--- main.ts
+++ main.ts
@@ -11,14 +11,32 @@ function load(url: string) {
         let xhr = new XMLHttpRequest();
 
         xhr.addEventListener("load", () => {
-            let data = JSON.parse(xhr.response);
-            observer.next(data);
-            observer.complete();
+            if (xhr.status === 200) {
+                let data = JSON.parse(xhr.response);
+                observer.next(data);
+                observer.complete();
+            } else {
+                observer.error(xhr.statusText);
+            }
         });
 
         xhr.open('GET', url);
         xhr.send();
-    });
+    // take an observabel and return an observable
+    }).retryWhen(retryStrategy({ attempts: 3, delay: 1500 }));
+}
+
+function retryStrategy({attempts = 4, delay = 1000}) {
+    return function(error) {
+        return error
+            .scan((acc, value) => {
+                console.log(acc, value);
+                return acc + 1;
+            }, 1)
+            // stop observable!!
+            .takeWhile((acc) => acc < attempts )
+            .delay(delay);
+    }
 }
 
 function renderMovies(movies) {
